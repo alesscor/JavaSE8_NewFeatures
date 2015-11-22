ccb39c6beadf6698ec4cd9a7d81bb6608c2ef32b
Alessandro Cordero <alesscor@gmail.com>
2015-11-22 09:44:09 -0600

    Simplifying the runnable interface's implementation

--- BuiltInInterfaces/src/org/example/java8/UseRunnable.java
+++ BuiltInInterfaces/src/org/example/java8/UseRunnable.java
@@ -4,22 +4,9 @@ public class UseRunnable {
 
 	public static void main(String[] args) {
 		
-		Runnable r1 = new Runnable() {
-			
-			@Override
-			public void run() {
-				System.out.println("Running Thread 1");
-			}
-		};
-		
-		Runnable r2 = new Runnable() {
-			
-			@Override
-			public void run() {
-				System.out.println("Running Thread 2");
-			}
-		};
-		
+		Runnable r1= () -> System.out.println("Running Thread 1");		
+		Runnable r2= () -> System.out.println("Running Thread 2");
+
 		new Thread(r1).start();
 		new Thread(r2).start();
 		
