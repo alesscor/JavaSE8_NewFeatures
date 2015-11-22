fd9c3c79cff9ec847c29925f9de97c08974bce6c
Alessandro Cordero <alesscor@gmail.com>
2015-11-22 10:05:02 -0600

    Simplifying the runnable interface's implementation

--- BuiltInInterfaces/src/org/example/java8/UseRunnable.java
+++ BuiltInInterfaces/src/org/example/java8/UseRunnable.java
@@ -3,13 +3,18 @@ package org.example.java8;
 public class UseRunnable {
 
 	public static void main(String[] args) {
-		
-		Runnable r1= () -> System.out.println("Running Thread 1");		
-		Runnable r2= () -> System.out.println("Running Thread 2");
-
-		new Thread(r1).start();
-		new Thread(r2).start();
-		
+		// the amount of code can be much less than the first version's 
+		new Thread(
+			() -> {
+				try {
+					Thread.sleep(1000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				System.out.println("Running Thread 1");			
+			}				
+		).start();
+		new Thread(() -> System.out.println("Running Thread 2")).start();
 	}
 
 }
