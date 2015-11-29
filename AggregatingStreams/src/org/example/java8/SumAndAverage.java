8e4fcda084349ea4d0d112f5a099df0fd775c315
Alessandro Cordero <alesscor@gmail.com>
2015-11-29 15:11:19 -0600

    Using aggregate functions to get sums and averages

--- AggregatingStreams/src/org/example/java8/SumAndAverage.java
+++ AggregatingStreams/src/org/example/java8/SumAndAverage.java
@@ -2,6 +2,7 @@ package org.example.java8;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 
 import org.example.java8.model.Person;
 
@@ -13,8 +14,17 @@ public class SumAndAverage {
 
 		people.add(new Person("Joe", 48));
 		people.add(new Person("Mary", 30));
+		people.add(new Person("Matusalem", 300));
 		people.add(new Person("Mike", 73));
-						
+		
+		int sum=people.stream().mapToInt(p -> p.getAge()).sum();
+		System.out.println("The sum of ages is: "+sum);
+		OptionalDouble avg=people.stream().mapToInt(p -> p.getAge()).average();
+		if(avg.isPresent()){
+			System.out.println("The average of ages is: "+avg.getAsDouble());
+		}else{
+			System.out.println("The average of ages is: zero");
+		}
 	}
 	
 }
