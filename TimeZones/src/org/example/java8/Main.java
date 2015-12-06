8dd678d8291a3ce3d61fcd4f7af5c57778cce30b
Alessandro Cordero <alesscor@gmail.com>
2015-12-06 17:36:38 -0600

    Continuing with new Date and Time API, using time zones

---
+++ TimeZones/src/org/example/java8/Main.java
@@ -0,0 +1,24 @@
+package org.example.java8;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+		LocalDateTime dt = LocalDateTime.now();
+		System.out.println(dtf.format(dt));
+		
+		ZonedDateTime gmt=ZonedDateTime.now(ZoneId.of("GMT+0"));
+		System.out.println("The GMT date time: "+gmt);
+
+		ZonedDateTime crct=ZonedDateTime.now(ZoneId.of("GMT-6"));
+		System.out.println("The Costa Rica date time: "+crct);
+	}
+
+}
