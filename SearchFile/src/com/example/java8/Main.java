26201426cbb6d2aced58a4539485e183d857977f
Alessandro Cordero <alesscor@gmail.com>
2015-12-08 18:47:35 -0600

    Learning on other improvements and tools that come with Java SE 8

--- SearchFile/src/com/example/java8/Main.java
+++ SearchFile/src/com/example/java8/Main.java
@@ -1,15 +1,27 @@
 package com.example.java8;
 
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		@SuppressWarnings("unused")
 		Path path = FileSystems.getDefault().getPath("files", "hamlet.txt");
-		
+		String searchTerm="To be, or not to be";
+		try (Stream<String> lines=Files.lines(path)) {
+			Optional<String> line=lines.filter(l -> l.contains(searchTerm)).findFirst();
+			if(line.isPresent()){
+				System.out.println("Found: "+line.get());
+			}else{
+				System.out.println("Not found");				
+			}
+		} catch (Exception e) {
+			System.out.println("There was an error "+e.getMessage());				
+		}
 	}		
 
 }
