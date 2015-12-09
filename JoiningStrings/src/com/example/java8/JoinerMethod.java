10b14e93edd3b4099499db4340d5ee9113a501a1
Alessandro Cordero <alesscor@gmail.com>
2015-12-08 18:19:28 -0600

    Learning on other improvements and tools that come with Java SE 8

---
+++ JoiningStrings/src/com/example/java8/JoinerMethod.java
@@ -0,0 +1,9 @@
+package com.example.java8;
+
+public class JoinerMethod {
+
+	public static void main(String[] args) {
+		String stooges=String.join(", ", "Larry","Curly","Moe");
+		System.out.println(stooges);
+	}		
+}
---
+++ JoiningStrings/src/com/example/java8/StringJoinerClass.java
@@ -0,0 +1,24 @@
+package com.example.java8;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class StringJoinerClass {
+	
+	public static void main(String[] args) {
+		
+//		Creating a StringJoiner
+
+		
+//		Merging StringJoiner instances
+
+		
+//		Working with collections
+		Set<String> set = new TreeSet<>();
+		set.add("California");
+		set.add("Oregon");
+		set.add("Washington");
+
+	}
+	
+}
